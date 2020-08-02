66f20b14bc3686c95283c0917fb20ee7f00df7cd
Tisya Banerjee <tisyabanerjee@gmail.com>
2020-08-02 15:59:31 +0530

    Course Project 1

--- plot4.R
+++ plot4.R
@@ -5,23 +5,28 @@ names(power) <- c("Date", "Time", "Global_active_power", "Global_reactive_power"
 #Subseting data based on date
 subpower <- subset(power, power$Date == "1/2/2007" | power$Date == "2/2/2007")
 #converting date to day
-newdate <- strptime(paste(subpower$Date, subpower$Time, sep = " "), "%d%m%Y %H:%M:%S")
+subpower$Date <- as.Date(subpower$Date, format="%d/%m/%Y")
+subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
+subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 submetering1 <- as.numeric(as.character(subpower$Sub_metering_1))
 submetering2 <- as.numeric(as.character(subpower$Sub_metering_2))
 submetering3 <- as.numeric(as.character(subpower$Sub_metering_3))
-Global_active_power <- as.numeric(as.character(subpower$Global_active_power))
-Global_reactive_power <- as.numeric(as.character(subpower$Global_reactive_power))
-Voltage <- as.numeric(as.character(subpower$Voltage))
-par(mfrow = c(2,2))
 #opening png device window
 png("plot4.png", width = 480, height = 480)
-plot(newdate, Global_active_power, ylab = "Global Active Power (kilowatts)")
-plot(newdate, Voltage, xlab = "datetime", ylab = "Voltage")
-plot(newdate, submetering1, type = "n", ylab = "Energy sub metering")
-with(subpower, lines(newdate, submetering1, type = 1))
-with(subpower, lines(newdate, submetering2, type = 1, col = "red"))
-with(subpower, lines(newdate, submetering3, type = 1, col = "blue"))
+par(mfrow = c(2,2))
+plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
+plot(subpower$Time, as.numeric(as.character(subpower$Voltage)), type = "l", xlab = "datetime", ylab = "Voltage")
+plot(subpower$Time, submetering1, type = "n", xlab = "", ylab = "Energy sub metering")
+lines(subpower$Time, submetering1)
+lines(subpower$Time, submetering2, col = "red")
+lines(subpower$Time, submetering3, col = "blue")
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, lwd = 2.5, col = c("black", "red", "blue"))
-plot(newdate, Global_reactive_power, xlab = "datetime", ylab = "Global_reactive_power")
+plot(subpower$Time, as.numeric(as.character(subpower$Global_reactive_power)), type = "l", xlab = "datetime", ylab = "Global_reactive_power")
 #closing device window
 dev.off()
+
+
+
+
+
