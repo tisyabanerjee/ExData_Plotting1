da01a362e63f0b6cd994eb45af4324e626acd4bc
Tisya Banerjee <tisyabanerjee@gmail.com>
2020-08-02 13:42:58 +0530

    plot2.R

--- plot2.R
+++ plot2.R
@@ -4,11 +4,9 @@ power <- read.table("household_power_consumption.txt", skip = 1, sep = ";")
 names(power) <- c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 #Subseting data based on date
 subpower <- subset(power, power$Date == "1/2/2007" | power$Date == "2/2/2007")
-png("plot1.png", width = 480, height = 480)
-hist(as.numeric(as.character(subpower$Global_active_power)), col = "red", xlab = "Global Active Power (kilowatts)")
-title(main = "Global Active Power")
-#closing evice window
+newdate <- strptime(paste(subpower$Date, subpower$Time, sep = " "), "%d%m%Y %H:%M:%S")
+Global_active_power <- as.numeric(as.character(subpower$Global_active_power))
+png("plot2.png", width = 480, height = 480)
+plot(newdate, Global_active_power, ylab = "Global Active Power (kilowatts)")
 dev.off()
 
-
-              
